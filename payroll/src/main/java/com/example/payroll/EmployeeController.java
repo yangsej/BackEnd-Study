3c8d2265cca6d1ff1b9ae99ae0f3b66bb7149778
yangsej <yangsej@gmail.com>
2021-04-01 18:43:09 +0900

    Spring Rest Tutorial 1

--- payroll/src/main/java/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/com/example/payroll/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.example.payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,18 +20,26 @@ class EmployeeController {
 
   private final EmployeeRepository repository;
 
-  EmployeeController(EmployeeRepository repository) {
+  private final EmployeeModelAssembler assembler;
+
+  EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
     this.repository = repository;
+    this.assembler = assembler;
   }
 
 
   // Aggregate root
   // tag::get-aggregate-root[]
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+  
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+        .map(assembler::toModel)
+        .collect(Collectors.toList());
+  
+    return CollectionModel.of(employees,
+    linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
-  // end::get-aggregate-root[]
 
   @PostMapping("/employees")
   Employee newEmployee(@RequestBody Employee newEmployee) {
@@ -40,13 +50,10 @@ class EmployeeController {
   @GetMapping("/employees/{id}")
   EntityModel<Employee> one(@PathVariable Long id) {
 
-  Employee employee = repository.findById(id) //
-      .orElseThrow(() -> new EmployeeNotFoundException(id));
+    Employee employee = repository.findById(id) //
+        .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-
-  return EntityModel.of(employee, //
-      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    return assembler.toModel(employee);
   }
 
   @PutMapping("/employees/{id}")
